e9d27bf3d95a28d92c1d259929d9625c54e3fe99
nmehanma <nmehanma@gmail.com>
2020-03-27 00:57:13 -0400

    updating form for credit card information and payment model in general

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_222725) do
+ActiveRecord::Schema.define(version: 2020_03_27_034219) do
+
+  create_table "payments", force: :cascade do |t|
+    t.string "email"
+    t.string "token"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
